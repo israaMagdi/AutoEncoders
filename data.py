dd3906131d7ded0c3c292630169ec617540a0338
israaMagdi <isra20354@feng.bu.edu.eg>
2025-09-27 14:34:02 +0300

    train and test

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ def preprocess(imgs):
     """
     imgs = imgs.astype("float32") / 255.0  # normalize
     imgs = np.pad(
-        imgs, ((0, 0), (2, 2), (3, 3)), constant_values=0.0
+        imgs, ((0, 0), (2, 2), (2, 2)), constant_values=0.0
     )  # padding to save the information on corners
     imgs = np.expand_dims(
         imgs, -1
--- model.py
+++ model.py
@@ -1,7 +1,11 @@
-from tensorflow.keras import layers, models
 
-# Input: 28x28x1
-input_img = layers.Input(shape=(28, 28, 1))
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+from data import load_transform_data
+
+# Input: 32x32x1
+input_img = layers.Input(shape=(32, 32, 1))
 
 # Encoder
 x = layers.Conv2D(32, (3, 3), activation="relu", padding="same")(input_img)
@@ -24,3 +28,27 @@ autoencoder = models.Model(input_img, decoded)
 
 autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
 autoencoder.summary()
+
+x_train, y_train, x_test, y_test = load_transform_data()
+history = autoencoder.fit(
+    x_train, x_train,   # input = output
+    epochs=20,
+    batch_size=256,
+    shuffle=True,
+    validation_data=(x_test, x_test)
+)
+
+decoded_imgs = autoencoder.predict(x_test[:10])
+
+plt.figure(figsize=(20, 4))
+for i in range(10):
+    # Original
+    ax = plt.subplot(2, 10, i + 1)
+    plt.imshow(x_test[i].reshape(32, 32), cmap="gray")
+    plt.axis("off")
+
+    # Reconstructed
+    ax = plt.subplot(2, 10, i + 1 + 10)
+    plt.imshow(decoded_imgs[i].reshape(32, 32), cmap="gray")
+    plt.axis("off")
+plt.show()
