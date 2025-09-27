963e4959af30418f4cfa34d94fb075f5d8a2c221
Ahmed Fawzy <ahmed.mohamed.fawzy.abdelfattah@gmail.com>
2025-09-27 11:08:45 +0300

    second update

--- data.py
+++ data.py
@@ -5,6 +5,9 @@ import numpy as np
 
 # Preprocessing
 def preprocess(imgs):
+    """
+    normalize, padding, handle the channels number for grayscale images
+    """
     imgs = imgs.astype("float32") / 255.0  # normalize
     imgs = np.pad(
         imgs, ((0, 0), (2, 2), (3, 3)), constant_values=0.0
@@ -27,3 +30,13 @@ def load_dataset():
 # Apply preprocessing on images
 # x_train = preprocess(x_train)
 # x_test = preprocess(x_test)
+
+
+def load_transform_data():
+    """ "
+    This function return the preprocessed data as x_train, y_train, x_test, y_test
+    """
+    x_train, y_train, x_test, y_test = load_dataset()
+    x_train = preprocess(x_train)
+    x_test = preprocess(x_test)
+    return x_train, y_train, x_test, y_test
